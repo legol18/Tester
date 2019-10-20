98a7cd0fdc7292984ef03b17a18d0de0b6a6426e
Rajat <legol18@gmail.com>
2019-10-20 21:07:32 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -7,26 +7,25 @@ echo 0.8 > 2.dat
 touch 3.dat
 echo -0.8 > 3.dat
 
-ifort random.f90 test_up.f90 -o tt -r8
+#### for testing
+# ifort random.f90 test_up.f90 -O2  -stand f03 -assume realloc_lhs  -check all  -traceback  -warn all  -fstack-protector  -assume protect_parens  -implicitnone -o tt
+
+gfortran random.f90 ref_prog.f90 -o tt -r8 
 ./tt < 1.dat
-cp fort.11 zero.dat
-cp fort.21 zero_sp_rk_p01.dat
-rm fort.11
-rm fort.21
+cp fort.11 zero_w.dat
 
 ./tt < 2.dat
-cp fort.11 p8.dat
-cp fort.21 p8_sp_rk_p01.dat
-rm fort.11
-rm fort.21
+cp fort.11 p8_w.dat
 
 ./tt < 3.dat
-cp fort.11 mp8.dat
-cp fort.21 mp8_sp_rk_p01.dat
-rm fort.11
+cp fort.11 mpzero6_w.dat
+
 rm 1.dat
 rm 2.dat
 rm 3.dat
+rm fort.*
 
 gnuplot plotting.gpl
+
 rm *.mod
+rm tt
